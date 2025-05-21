44b26411c3e22a26687a36b951220871b52b90d5
Mark Story <mark@mark-story.com>
2025-05-21 00:30:16 -0400

    Get basic forms styled better

--- config/form-templates.php
+++ config/form-templates.php
@@ -6,8 +6,11 @@ declare(strict_types=1);
  */
 return [
     'button' => '<button class="bg-red-800 p-2 text-white rounded-sm"{{attrs}}>{{text}}</button>',
-    'input' => '<input class="border border-slate-500 rounded-sm" type="{{type}}" name="{{name}}"{{attrs}}>',
+    'checkbox' => '<input class="mr-2" type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}>',
+    'input' => '<input class="border border-slate-500 rounded-sm max-w-md min-w-4" type="{{type}}" name="{{name}}"{{attrs}}>',
     'inputContainer' => '<div class="py-3 flex gap-2 justify-between">{{content}}</div>',
-    'textarea' => '<textarea class="border border-slate-500 rounded-sm" name="{{name}}"{{attrs}}>{{value}}</textarea>',
-    'select' => '<select class="border border-slate-500 rounded-sm" name="{{name}}"{{attrs}}>{{content}}</select>',
+    'label' => '<label class="mr-2" {{attrs}}>{{text}}</label>',
+    'textarea' => '<textarea class="border border-slate-500 rounded-sm max-w-md min-w-4" name="{{name}}"{{attrs}}>{{value}}</textarea>',
+    'select' => '<select class="border border-slate-500 rounded-sm max-w-md min-w-4" name="{{name}}"{{attrs}}>{{content}}</select>',
+    'selectMultiple' => '<select class="border border-slate-500 rounded-sm max-w-md min-w-50" name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
 ];

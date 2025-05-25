c27dc86a550791baf51fb0b7d77c1a2790eec4e2
Mark Story <mark@mark-story.com>
2025-05-24 23:33:33 -0400

    Improve index and view templates

---
+++ config/html-templates.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'link' => '<a href="{{url}}" class="hover:text-red-800" {{attrs}}>{{content}}</a>',
+];
--- config/paginator-templates.php
+++ config/paginator-templates.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
  * Custom templates for pagination elements.
  */
 return [
-    'nextActive' => '<li class="next"><a rel="next" href="{{url}}">{{text}}</a></li>',
+    'nextActive' => '<li class=""><a rel="next" href="{{url}}">{{text}}</a></li>',
     'nextDisabled' => '<li class="next text-gray-400"><a>{{text}}</a></li>',
-    'prevActive' => '<li class="prev"><a rel="prev" href="{{url}}">{{text}}</a></li>',
+    'prevActive' => '<li class="text-red-700"><a rel="prev" href="{{url}}">{{text}}</a></li>',
     'prevDisabled' => '<li class="prev text-gray-400"><a>{{text}}</a></li>',
     'counterRange' => '{{start}} - {{end}} of {{count}}',
     'counterPages' => '{{page}} of {{pages}}',
@@ -16,9 +16,9 @@ return [
     'number' => '<li><a href="{{url}}">{{text}}</a></li>',
     'current' => '<li class="active"><a href="">{{text}}</a></li>',
     'ellipsis' => '<li class="ellipsis">&hellip;</li>',
-    'sort' => '<a href="{{url}}">{{text}}</a>',
-    'sortAsc' => '<a class="asc" href="{{url}}">{{text}}</a>',
-    'sortDesc' => '<a class="desc" href="{{url}}">{{text}}</a>',
+    'sort' => '<a class="hover:text-red-800" href="{{url}}">{{text}}</a>',
+    'sortAsc' => "<a class=\"whitespace-nowrap text-red-700 after:text-red-700 after:content-['_↓']\" href=\"{{url}}\">{{text}}</a>",
+    'sortDesc' => "<a class=\"whitespace-nowrap text-red-700 after:text-red-700 after:content-['_↑']\" href=\"{{url}}\">{{text}}</a>",
     'sortAscLocked' => '<a class="asc locked" href="{{url}}">{{text}}</a>',
     'sortDescLocked' => '<a class="desc locked" href="{{url}}">{{text}}</a>',
 ];

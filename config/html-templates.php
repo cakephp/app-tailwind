528b47b32881da7904badcbd03396e76d0573db4
Mark Story <mark@mark-story.com>
2025-05-30 00:04:54 -0400

    Default link class was a mistake

--- config/html-templates.php
+++ config/html-templates.php
@@ -2,5 +2,5 @@
 declare(strict_types=1);
 
 return [
-    'link' => '<a href="{{url}}" class="hover:text-red-800" {{attrs}}>{{content}}</a>',
+    'link' => '<a href="{{url}}" {{attrs}}>{{content}}</a>',
 ];

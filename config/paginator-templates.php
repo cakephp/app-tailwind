cefbcfe4eb7b5ed3109148b95a6185f4787751e3
Mark Story <mark@mark-story.com>
2025-05-30 00:21:49 -0400

    More template improvements.

--- config/paginator-templates.php
+++ config/paginator-templates.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
  * Custom templates for pagination elements.
  */
 return [
-    'nextActive' => '<li class=""><a rel="next" href="{{url}}">{{text}}</a></li>',
+    'nextActive' => '<li class=""><a class="hover:text-red-800" rel="next" href="{{url}}">{{text}}</a></li>',
     'nextDisabled' => '<li class="next text-gray-400"><a>{{text}}</a></li>',
-    'prevActive' => '<li class="text-red-700"><a rel="prev" href="{{url}}">{{text}}</a></li>',
+    'prevActive' => '<li class="text-red-800"><a rel="prev" href="{{url}}">{{text}}</a></li>',
     'prevDisabled' => '<li class="prev text-gray-400"><a>{{text}}</a></li>',
     'counterRange' => '{{start}} - {{end}} of {{count}}',
     'counterPages' => '{{page}} of {{pages}}',
-    'first' => '<li class="first"><a href="{{url}}">{{text}}</a></li>',
-    'last' => '<li class="last"><a href="{{url}}">{{text}}</a></li>',
-    'number' => '<li><a href="{{url}}">{{text}}</a></li>',
-    'current' => '<li class="active"><a href="">{{text}}</a></li>',
+    'first' => '<li class="first"><a class="hover:text-red-800" href="{{url}}">{{text}}</a></li>',
+    'last' => '<li class="last"><a class="hover:text-red-800" href="{{url}}">{{text}}</a></li>',
+    'number' => '<li><a class="hover:text-red-800" href="{{url}}">{{text}}</a></li>',
+    'current' => '<li class="active"><a class="text-red-600 hover:text-red-800" href="">{{text}}</a></li>',
     'ellipsis' => '<li class="ellipsis">&hellip;</li>',
     'sort' => '<a class="hover:text-red-800" href="{{url}}">{{text}}</a>',
     'sortAsc' => "<a class=\"whitespace-nowrap text-red-700 after:text-red-700 after:content-['_↓']\" href=\"{{url}}\">{{text}}</a>",
